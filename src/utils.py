995e267a2b7a9e8365ed433cb9b8a45c6bfb8051
ocedev112 <olewuenyie@gmail.com>
2025-08-12 16:39:49 +0100

    Added new transformers variation

--- src/utils.py
+++ src/utils.py
@@ -5,13 +5,15 @@ import matplotlib.pyplot as plt
 
 def get_transforms():
 
-    return transforms.Compose([
-        transforms.Resize((224,224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                           std=[0.229, 0.224, 0.225]) 
-    ])
-
+        return transforms.Compose([
+            transforms.Resize((300, 300)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2),
+            transforms.Grayscale(num_output_channels =3),
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+        ])
 
 def calculate_accuracy(y_pred, y_true):
     _, preds = torch.max(y_pred, 1)
